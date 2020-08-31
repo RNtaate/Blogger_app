55cb40b4de6a2fc89436ab12cbcb6d034d93b424
RNtaate <royhyde14@gmail.com>
2020-08-31 18:46:46 +0300

    Add view templates of new and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,15 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+  end
 end
