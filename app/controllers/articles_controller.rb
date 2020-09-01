032ccfb72d23ea3c15ff4118b8c9a47c83463386
RNtaate <royhyde14@gmail.com>
2020-09-01 13:17:09 +0300

    Add edit and update methods to article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,11 +15,32 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
+    flash.notice = "Article '#{@article.title}' successfully Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
-    Article.destroy(params[:id])
+    @article = Article.find(params[:id])
+    flash.notice = "Article '#{@article.title}' successfully Deleted!"
+
+    @article.destroy
+
     redirect_to articles_path
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+  
+    redirect_to article_path(@article)
+  end
+  
 end
