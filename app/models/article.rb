3cd012d7fe64108c51033950e32e9000b0d18a75
RNtaate <royhyde14@gmail.com>
2020-09-01 18:46:46 +0300

    Add interface for tagging articles, add tag controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,10 @@ class Article < ApplicationRecord
   def tag_list
     tags.join(", ")
   end
+
+  def tag_list=(tag_list)
+    tag_names = tag_list.strip.downcase.split(", ").uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags  
+  end
 end
