efc8f9fd5c9c48fee17b3e4f52ee2933cfca68fa
ershadul1 <ershadulrayhan@gmail.com>
2020-09-02 15:30:05 +0600

    Add listing all tags page and delete tag functionality

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,4 +4,13 @@ class TagsController < ApplicationController
     @tag = Tag.find(params[:id])
   end
   
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to tags_path
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   has_many :taggings
-  has_many :articles, through: :taggings
+  has_many :articles, through: :taggings, dependent: :destroy
 
   def to_s
     name
