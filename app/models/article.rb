780a07710abab12e067f89634c97987a3f6b024b
RNtaate <royhyde14@gmail.com>
2020-09-02 16:31:42 +0300

    Add image automatic resizing funcitonality

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,8 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
 
-  has_attached_file :image
+  has_attached_file :image, styles: { medium: "700x700>", thumb: "100x100>" }
+
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 
